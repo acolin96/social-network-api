8d3ac99496555411de72ec34fbf60d69bbb5ee75
Alex Colin <acolin3196@gmail.com>
2023-07-13 20:49:33 -0500

    gatherthoughts and gathering a single thought finished

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,25 @@
 const {User, Thoughts} = require('../models/');
+
+module.exports = {
+    async gatherThoughts(req, res) {
+        try {
+            const thoughts = await Thoughts.find();
+            res.status(200).json(thoughts);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    async gatherOneThought(req, res) {
+        try {
+        const thought = await Thoughts.findOne({ _id: req.params.thoughtId })
+            .select('-__v');
+        if (!thought) {
+            return res.status(404).json({ message: 'No comment found' });
+        }
+        res.status(200).json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+ }
+}
